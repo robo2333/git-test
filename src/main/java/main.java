0c4ca673933a852580d0cdadadc84eb366d3d099
robo <robo@gmail.com>
2023-07-28 14:55:29 +0800

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName : main  //类名
+ * @Description :   //描述
+ * @Author : Rui //作者
+ * @Date: 2023/7/28  14:52
+ */
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
