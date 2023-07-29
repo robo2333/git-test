fc537d17ac321e8e0a5cd091cb37d8fae278e17f
robo <robo@gmail.com>
2023-07-30 03:38:18 +0800

    hot-fix first commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,5 +9,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好123");
+        System.out.println("你好git4");
+        System.out.println("你好git3");
     }
 }
